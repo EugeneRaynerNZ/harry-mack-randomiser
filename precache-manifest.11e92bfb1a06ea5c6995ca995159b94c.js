8c0695885bf5908defc1fc2968e8a4e98b1350c5
EugeneRaynerNZ <rayner.euge@gmail.com>
2020-10-09 11:51:51 +1300

    Updates

---
+++ precache-manifest.11e92bfb1a06ea5c6995ca995159b94c.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9ae34aac34e5d685e6978bc6d73258b9",
+    "url": "/harry-mack-randomiser/index.html"
+  },
+  {
+    "revision": "aaacaa56b8b70f14b161",
+    "url": "/harry-mack-randomiser/static/css/2.d9ad5f5c.chunk.css"
+  },
+  {
+    "revision": "e928ef28cdee63e728ee",
+    "url": "/harry-mack-randomiser/static/css/main.728c0f14.chunk.css"
+  },
+  {
+    "revision": "aaacaa56b8b70f14b161",
+    "url": "/harry-mack-randomiser/static/js/2.4ff57aef.chunk.js"
+  },
+  {
+    "revision": "d837bdfa2a2c08de9ff5012f63aaaf8c",
+    "url": "/harry-mack-randomiser/static/js/2.4ff57aef.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e928ef28cdee63e728ee",
+    "url": "/harry-mack-randomiser/static/js/main.8c2babe7.chunk.js"
+  },
+  {
+    "revision": "fd03fef09f105c437817",
+    "url": "/harry-mack-randomiser/static/js/runtime-main.fbb232ed.js"
+  }
+]);
